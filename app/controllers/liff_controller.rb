94884fae6c4c9f06690ca35a01faadea423eca3f
etrex kuo <et284vu065k3@gmail.com>
2022-01-09 15:50:22 +0800

    add decoded_id_token

--- app/controllers/liff_controller.rb
+++ app/controllers/liff_controller.rb
@@ -23,7 +23,7 @@ class LiffController < ActionController::Base
     # fix liff 2.0 redirect issue
     @need_reload = query["liff.state"].present?
 
-    # 需要從 session 讀取 @liff_param
+    # 需要從 session 讀取 @liff_param (使用電腦開啟 LIFF 頁面，第一次登入時的情況)
     if query["liffRedirectUri"].present?
       @liff_param = Base64DecodeService.new(session[:liff_param]).run
       session[:liff_param] = nil
@@ -75,22 +75,53 @@ class LiffController < ActionController::Base
     res&.to_s || e.full_message
   end
 
+
+  # profile format
+  # {
+  #   "userId"=>"U1234567890abcdefghijklmnopqrstuv",
+  #   "displayName"=>"user name",
+  #   "statusMessage"=>"user status message",
+  #   "pictureUrl"=>"https://this.is.an.image.url"
+  # }
   def source_info
     context = params["context"]
-    return nil if context.nil?
-
-    source_type = context["type"].gsub("utou", "user")
-    source_group_id = context["roomId"] || context["groupId"] || context["userId"]
-    source_user_id = context["userId"]
+    return {} if context.nil?
 
     profile = params["profile"]
+    return {} if profile.nil?
+
+    # authorize the user id
+    decoded_id_token = get_decoded_id_token
+    return {} unless decoded_id_token["sub"] == profile["userId"] && decoded_id_token["sub"] == profile["userId"]
+
+    source_type = context["type"].gsub("utou", "user")
+    source_group_id = context["roomId"] || context["groupId"] || decoded_id_token["sub"]
+    source_user_id = decoded_id_token["sub"]
 
     {
       platform_type: 'line',
       source_type: source_type,
       source_group_id: source_group_id,
       source_user_id: source_user_id,
-      profile: profile
+      context: context,
+      profile: profile,
+      decoded_id_token: decoded_id_token
     }
   end
+
+  # decoded_id_token is JWT format
+  # {
+  #  "iss"=>"https://access.line.me",
+  #  "sub"=>"U1234567890abcdefghijklmnopqrstuv",
+  #  "aud"=>"1234567890",
+  #  "exp"=>1641638901,
+  #  "iat"=>1641635301,
+  #  "name"=>"user name",
+  #  "picture"=>"https://this.is.an.image.url"
+  # }
+  def get_decoded_id_token
+    id_token = params["idToken"]
+    Kamiliff.line_login_client.verify_id_token(id_token: id_token)
+  end
+
 end
--- lib/kamiliff.rb
+++ lib/kamiliff.rb
@@ -2,7 +2,50 @@ require "kamiliff/engine"
 require "kamiliff/services/base64_encode_service"
 require "kamiliff/services/base64_decode_service"
 require "kamiliff/services/liff_service"
+require "line_login"
 
 module Kamiliff
-  # Your code goes here...
+  # env
+  mattr_writer :line_login_channel_id
+  mattr_writer :line_login_channel_secret
+  mattr_writer :line_login_redirect_uri
+
+  mattr_writer :liff_url_compact
+  mattr_writer :liff_url_tall
+  mattr_writer :liff_url_full
+
+  def self.line_login_channel_id
+    @@line_login_channel_id || ENV["LINE_LOGIN_CHANNEL_ID"]
+  end
+
+  def self.line_login_channel_secret
+    @@line_login_channel_secret || ENV["LINE_LOGIN_CHANNEL_SECRET"]
+  end
+
+  def self.line_login_redirect_uri
+    @@line_login_redirect_uri || ENV["LINE_LOGIN_REDIRECT_URI"]
+  end
+
+  def self.liff_url_compact
+    @@liff_url_compact || ENV["LIFF_COMPACT"]
+  end
+
+  def self.liff_url_tall
+    @@liff_url_tall || ENV["LIFF_TALL"]
+  end
+
+  def self.liff_url_full
+    @@liff_url_full || ENV["LIFF_FULL"]
+  end
+
+  def self.line_login_client
+    @line_login_client ||= LineLogin::Client.new(
+      client_id: Kamiliff.line_login_channel_id,
+      client_secret: Kamiliff.line_login_channel_secret
+    )
+  end
+
+  def self.setup
+    yield self
+  end
 end
